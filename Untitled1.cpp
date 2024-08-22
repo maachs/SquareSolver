24f28325761958bcd979498d8e30a4ec97c6067e
maachs <maxvufa@yandex.ru>
2024-08-22 20:37:57 +0300

    с тестом

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -13,7 +13,7 @@ enum Compare
 
 enum KolvoResh
 {
-    INF = -1,
+    INF_ROOTS = -1,
     ONE_ROOTS = 1,
     NO_ROOTS = 0,
     TWO_ROOTS = 2,
@@ -28,74 +28,66 @@ struct Coeffs
 
 struct Roots
 {
-  double x1;
-  double x2;
-  int nRoots;
+  double x1 = NAN;
+  double x2 = NAN;
+  int nRoots = NAN;
+};
+struct ExpectedSol
+{
+    double x1_expected;
+    double x2_expected;
+    int nRoots_expected;
+    int nTest;
 };
 
 void OutputSolution (Roots solution);
 
 void InputCoeffs (Coeffs* coeffs);
 
-int SolveSquare (Coeffs coeffs, Roots* solution);
-
-int RT (double a, double b, double c, double x1e, double x2e, int nRootsE, int nTest);
+void SolveSquare (Coeffs coeffs, Roots* solution);
 
 int CheckCoeffs (Coeffs coeffs);
 
 int CompareOnZero (double compare);
 
+void LineSolution (Coeffs coeffs, Roots* solution);
+
+void SquareSolution (Coeffs coeffs, Roots* solution);
+
+int Test (Coeffs coeffs,Roots* solution, ExpectedSol* expected);
+
 int main()
 {
-    Coeffs coeffs;
-    Roots solution;
+    Coeffs coeffs = {.a = 1,.b = 2,.c = 1};
 
-    InputCoeffs(&coeffs);
-    //RT(a, b, c, x1e, x2e, nRootsE, nTest
-    solution.nRoots = SolveSquare(coeffs,  &solution);
-    OutputSolution(solution);
+    Roots solution = {};
+
+    ExpectedSol expected = {-1,-1,1,1};
+
+    //InputCoeffs(&coeffs);
+    SolveSquare(coeffs,  &solution);
+    Test (coeffs, &solution, &expected);
+    //OutputSolution(solution);
 
     return 0;
 }
 
-int SolveSquare(Coeffs coeffs, Roots* solution)
+void SolveSquare(Coeffs coeffs, Roots* solution)
 
 {
     assert (solution != NULL);
 
     if (CompareOnZero(coeffs.a) == 0)
     {
-        if (CompareOnZero(coeffs.b) == 0)
-        {
-            return (CompareOnZero(coeffs.c) == 0)? INF : 0;
-        }
-        else
-        {
-        solution->x1 = -coeffs.c/coeffs.b;
-        return ONE_ROOTS;
-        }
+          LineSolution (coeffs, solution);
+
     }
     else
     {
-        double d = coeffs.b * coeffs.b - 4 * coeffs.a *coeffs.c;
-        if (d < EPS||d > (-EPS))
-        {
-            solution->x1 = -coeffs.b/(2*coeffs.a);
-
-            return ONE_ROOTS;
-        }
-        else if (d > EPS)
-        {
-            solution->x1 = (-(coeffs.b) + sqrt(d))/(2*coeffs.a);
-            solution->x2 = (-(coeffs.b) - sqrt(d))/(2*coeffs.a);
-
-             return TWO_ROOTS;
-        }
-        else if (d < -EPS)
-        {
-            return NO_ROOTS;
-        }
+         SquareSolution (coeffs, solution);
     }
+
+
 }
 
 void OutputSolution (Roots solution)
@@ -103,7 +95,7 @@ void OutputSolution (Roots solution)
     switch (solution.nRoots)
     {
         case NO_ROOTS:
-            printf("РќРµС‚ СЂРµС€РµРЅРёР№");
+            printf("Нет решений");
             break;
         case ONE_ROOTS:
             printf("x = %lg", solution.x1);
@@ -111,8 +103,8 @@ void OutputSolution (Roots solution)
         case TWO_ROOTS:
             printf("x1 = %lg\nx2 = %lg", solution.x1, solution.x2);
             break;
-        case INF:
-            printf("Р»СЋР±РѕРµ С‡РёСЃР»Рѕ");
+        case INF_ROOTS:
+            printf("любое число");
             break;
         default:
             printf("error %d", solution.nRoots);
@@ -124,14 +116,15 @@ void OutputSolution (Roots solution)
 void InputCoeffs(Coeffs* coeffs)
 {
     assert (coeffs != NULL);
+
     while (true){
-        printf("Р’РІРµРґРёС‚Рµ РєРѕСЌС„С„РёС†РµРЅС‚С‹:\n");
+        printf("Введите коэффиценты:\n");
 
         if (scanf("%lg %lg %lg", &coeffs->a, &coeffs->b, &coeffs->c) != 3)
         {
 
             while (getchar() != '\n'){};
-            printf("РќРµРІРµСЂРЅРѕРµ Р·РЅР°С‡РµРЅРёРµ\n");
+            printf("Неверное значение\n");
             continue;
         }
 
@@ -156,7 +149,7 @@ int CheckCoeffs (Coeffs coeffs)
 
 int CompareOnZero (double compare)
 {
-    if (compare < EPS && compare >(-EPS))
+    if (fabs(compare) < EPS)
     {
         return EQUAL_TO_ZERO;
     }
@@ -164,8 +157,69 @@ int CompareOnZero (double compare)
     {
         return GREATER_THEN_ZERO;
     }
-    else if (compare < EPS)
+    // (compare < -EPS)
+
+    return LOWER_THAN_ZERO;
+}
+
+void LineSolution (Coeffs coeffs, Roots* solution)
+{
+
+        if (CompareOnZero(coeffs.b) == 0)
+        {
+            if (CompareOnZero(coeffs.c) == 0)
+            {
+                  solution->nRoots = INF_ROOTS;
+            }
+            else
+            {
+                solution->nRoots = NO_ROOTS;
+            }
+        }
+        else
+        {
+            solution->x1 = -coeffs.c/coeffs.b;
+            solution->nRoots = ONE_ROOTS;
+        }
+
+
+}
+
+void SquareSolution (Coeffs coeffs, Roots* solution)
+{
+    double d = coeffs.b * coeffs.b - 4 * coeffs.a * coeffs. c;
+
+    if (CompareOnZero(d) == 1)
     {
-        return LOWER_THAN_ZERO;
+        solution->x1 = (-(coeffs.b) + sqrt(d))/(2*coeffs.a);
+        solution->x2 = (-(coeffs.b) - sqrt(d))/(2*coeffs.a);
+        solution->nRoots = TWO_ROOTS;
+    }
+    else if (CompareOnZero(d) == 0)
+    {
+        solution->x1 = solution->x2 = (-coeffs.b)/(2*coeffs.a);
+        solution->nRoots = ONE_ROOTS;
+    }
+    else if (CompareOnZero(d) == -1)
+    {
+        solution->nRoots = NO_ROOTS;
     }
 }
+
+int Test (Coeffs coeffs,Roots* solution, ExpectedSol* expected )
+{
+    double delta_x1 = fabs((*solution).x1 - (*expected).x1_expected);
+    double delta_x2 = fabs((*solution).x2 - (*expected).x2_expected);
+    if ((*solution).nRoots != (*expected).nRoots_expected||CompareOnZero(delta_x1) != 0||CompareOnZero(delta_x2) != 0)
+    {
+        printf("Error: Test %d\n  a = %lg b = %lg c = %lg\n  x1 = %lg x2 = %lg nRoots = %d\n"
+               "Expected: x1 = %lg x2 = %lg nRoots = %d",
+               (*expected).nTest, coeffs.a, coeffs.b, coeffs.c, (*solution).x1, (*solution).x2, (*solution).nRoots,
+               (*expected).x1_expected, (*expected).x2_expected, (*expected).nRoots_expected);
+    }
+    else
+    {
+        printf ("Test %d Completed Succesfull",(*expected).nTest);
+    }
+}
+
