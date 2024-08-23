94b099ba374d5b4f4a8d83e83e4ced5eac932bba
maachs <maxvufa@yandex.ru>
2024-08-23 18:08:08 +0300

    со всеми тестами

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,5 +1,4 @@
 #include <TXLib.h>
-#include <stdio.h>
 #include <math.h>
 
 const double EPS = 0.001;
@@ -7,7 +6,7 @@ const double EPS = 0.001;
 enum Compare
 {
     EQUAL_TO_ZERO = 0,
-    GREATER_THEN_ZERO = 1,
+    GREATER_THAN_ZERO = 1,
     LOWER_THAN_ZERO = -1,
 };
 
@@ -25,7 +24,6 @@ struct Coeffs
     double b;
     double c;
 };
-
 struct Roots
 {
   double x1 = NAN;
@@ -34,10 +32,10 @@ struct Roots
 };
 struct ExpectedSol
 {
+    Coeffs coeffs;
     double x1_expected;
     double x2_expected;
     int nRoots_expected;
-    int nTest;
 };
 
 void OutputSolution (Roots solution);
@@ -54,28 +52,30 @@ void LineSolution (Coeffs coeffs, Roots* solution);
 
 void SquareSolution (Coeffs coeffs, Roots* solution);
 
-int Test (Coeffs coeffs,Roots* solution, ExpectedSol* expected);
+void Test (int TestNumber, Roots* solution, ExpectedSol* expected);
+
+void AllTest ();
+
+bool NAN_OR_INF (double check);
 
 int main()
 {
-    Coeffs coeffs = {.a = 1,.b = 2,.c = 1};
-
-    Roots solution = {};
-
-    ExpectedSol expected = {-1,-1,1,1};
+    //Coeffs coeffs = {.a = 1,.b = 2,.c = 1};
+    //Roots solution = {};
+    //ExpectedSol_1 expected = {1,1,NAN,1};
 
     //InputCoeffs(&coeffs);
-    SolveSquare(coeffs,  &solution);
-    Test (coeffs, &solution, &expected);
+    //SolveSquare (*expected_2);
+    AllTest ();
+    //Test (coeffs, &solution, &expected);
     //OutputSolution(solution);
 
     return 0;
 }
 
 void SolveSquare(Coeffs coeffs, Roots* solution)
-
 {
-    assert (solution != NULL);
+    assert (solution );
 
     if (CompareOnZero(coeffs.a) == 0)
     {
@@ -86,8 +86,6 @@ void SolveSquare(Coeffs coeffs, Roots* solution)
     {
          SquareSolution (coeffs, solution);
     }
-
-
 }
 
 void OutputSolution (Roots solution)
@@ -95,7 +93,7 @@ void OutputSolution (Roots solution)
     switch (solution.nRoots)
     {
         case NO_ROOTS:
-            printf("Нет решений");
+            printf("Нет решений\n");
             break;
         case ONE_ROOTS:
             printf("x = %lg", solution.x1);
@@ -104,7 +102,7 @@ void OutputSolution (Roots solution)
             printf("x1 = %lg\nx2 = %lg", solution.x1, solution.x2);
             break;
         case INF_ROOTS:
-            printf("любое число");
+            printf("Любое число");
             break;
         default:
             printf("error %d", solution.nRoots);
@@ -115,16 +113,15 @@ void OutputSolution (Roots solution)
 
 void InputCoeffs(Coeffs* coeffs)
 {
-    assert (coeffs != NULL);
+    assert (coeffs);
 
     while (true){
-        printf("Введите коэффиценты:\n");
+        printf("Введите коффиценты:\n");
 
         if (scanf("%lg %lg %lg", &coeffs->a, &coeffs->b, &coeffs->c) != 3)
         {
-
-            while (getchar() != '\n'){};
-            printf("Неверное значение\n");
+            while (getchar() != '\n') {};
+            printf("Неверные коэффиценты\n");
             continue;
         }
 
@@ -139,7 +136,7 @@ void InputCoeffs(Coeffs* coeffs)
 
 int CheckCoeffs (Coeffs coeffs)
 {
-    if (!isfinite(coeffs.a)||!isfinite(coeffs.b)||!isfinite(coeffs.c))
+    if (!isfinite(coeffs.a) || !isfinite(coeffs.b) || !isfinite(coeffs.c))
     {
         return 1;
     }
@@ -155,7 +152,7 @@ int CompareOnZero (double compare)
     }
     else if (compare > EPS)
     {
-        return GREATER_THEN_ZERO;
+        return GREATER_THAN_ZERO;
     }
     // (compare < -EPS)
 
@@ -164,6 +161,7 @@ int CompareOnZero (double compare)
 
 void LineSolution (Coeffs coeffs, Roots* solution)
 {
+    assert (solution);
 
         if (CompareOnZero(coeffs.b) == 0)
         {
@@ -174,52 +172,115 @@ void LineSolution (Coeffs coeffs, Roots* solution)
             else
             {
                 solution->nRoots = NO_ROOTS;
+                solution->x1 = NAN;
+                solution->x2 = NAN;
             }
         }
         else
         {
             solution->x1 = -coeffs.c/coeffs.b;
+            solution->x2 = NAN;
             solution->nRoots = ONE_ROOTS;
         }
-
-
 }
 
 void SquareSolution (Coeffs coeffs, Roots* solution)
 {
     double d = coeffs.b * coeffs.b - 4 * coeffs.a * coeffs. c;
 
-    if (CompareOnZero(d) == 1)
+    if (CompareOnZero(d) == GREATER_THAN_ZERO)
     {
-        solution->x1 = (-(coeffs.b) + sqrt(d))/(2*coeffs.a);
-        solution->x2 = (-(coeffs.b) - sqrt(d))/(2*coeffs.a);
+        solution->x1 = (-(coeffs.b) - sqrt(d))/(2*coeffs.a);
+        solution->x2 = (-(coeffs.b) + sqrt(d))/(2*coeffs.a);
         solution->nRoots = TWO_ROOTS;
     }
-    else if (CompareOnZero(d) == 0)
+    else if (CompareOnZero(d) == EQUAL_TO_ZERO)
     {
-        solution->x1 = solution->x2 = (-coeffs.b)/(2*coeffs.a);
+        solution->x1 = (-coeffs.b)/(2*coeffs.a);
+        solution->x2 = NAN;
         solution->nRoots = ONE_ROOTS;
     }
-    else if (CompareOnZero(d) == -1)
+    else if (CompareOnZero(d) == LOWER_THAN_ZERO)
     {
         solution->nRoots = NO_ROOTS;
+        solution->x1 = NAN;
+        solution->x2 = NAN;
     }
 }
 
-int Test (Coeffs coeffs,Roots* solution, ExpectedSol* expected )
+void Test (int TestNumber, Roots* solution, ExpectedSol* expected )
 {
-    double delta_x1 = fabs((*solution).x1 - (*expected).x1_expected);
-    double delta_x2 = fabs((*solution).x2 - (*expected).x2_expected);
-    if ((*solution).nRoots != (*expected).nRoots_expected||CompareOnZero(delta_x1) != 0||CompareOnZero(delta_x2) != 0)
+    SolveSquare (expected->coeffs, solution);
+    double delta_x1 = 0, delta_x2 = 0;
+
+    if (NAN_OR_INF(solution->x1) == 0)
     {
-        printf("Error: Test %d\n  a = %lg b = %lg c = %lg\n  x1 = %lg x2 = %lg nRoots = %d\n"
-               "Expected: x1 = %lg x2 = %lg nRoots = %d",
-               (*expected).nTest, coeffs.a, coeffs.b, coeffs.c, (*solution).x1, (*solution).x2, (*solution).nRoots,
+        if(NAN_OR_INF(expected->x1_expected) == 0)
+        {
+            delta_x1 = 0;
+        }
+    }
+    else
+    {
+        delta_x1 = fabs(solution->x1 - expected->x1_expected);
+    }
+
+    if (NAN_OR_INF(solution->x2) == 0)
+    {
+        if(NAN_OR_INF(expected->x2_expected) == 0)
+        {
+            delta_x2 = 0;
+        }
+    }
+    else
+    {
+        delta_x2 = fabs(solution->x2 - expected->x2_expected);
+    }
+      if ((*solution).nRoots != (*expected).nRoots_expected || CompareOnZero(delta_x1) != EQUAL_TO_ZERO || CompareOnZero(delta_x2) != EQUAL_TO_ZERO)
+    {
+        printf("\nError: Test %d\n  a = %lg b = %lg c = %lg\n  x1 = %lg x2 = %lg nRoots = %d\n"
+               "Expected:\n  x1 = %lg x2 = %lg nRoots = %d\n",
+               TestNumber + 1, expected->coeffs.a, expected->coeffs.b, expected->coeffs.c, (*solution).x1, (*solution).x2, (*solution).nRoots,
                (*expected).x1_expected, (*expected).x2_expected, (*expected).nRoots_expected);
     }
     else
     {
-        printf ("Test %d Completed Succesfull",(*expected).nTest);
+        printf ("Test %d Completed Succesfull\n",TestNumber + 1);
     }
 }
 
+void AllTest ()
+{
+    const int AmountOfTest = 11;
+    Roots solution = {};
+    ExpectedSol expected [AmountOfTest] = {{{0, 0, 0}, NAN, NAN, INF_ROOTS},
+                                           {{0, 0, 1}, NAN, NAN, 0},
+                                           {{0, 1, 0}, 0, NAN, 1},
+                                           {{1, 0, 0}, 0, NAN, 1},
+                                           {{0, 1, 1}, -1, NAN, 1},
+                                           {{1, 0, 1}, NAN, NAN, 0},
+                                           {{1, 1, 0}, -1, 0, 2},
+                                           {{1, 1, -12}, -4, 3, 2},
+                                           {{1, 1, 0.25}, -0.5, NAN, 1},
+                                           {{4, -3, 2}, NAN, NAN, 0},
+                                           {{2, 6, -3}, -3.43649, 0.436492,2}};
+
+    for (int TestNumber = 0; TestNumber < AmountOfTest; TestNumber++)
+    {
+        printf("Starts test %d\n", TestNumber + 1);
+        Test (TestNumber, &solution, expected + TestNumber);
+
+    }
+}
+
+bool NAN_OR_INF (double check)
+{
+    if (!isfinite(check))
+    {
+        return 0;    // NAN or INF
+    }
+    else
+    {
+        return 1;
+    }
+}
